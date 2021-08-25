87a33a6dad19ef8056e37695c4f6432698697305
unknown <alejosa1919@gmail.com>
2021-08-24 21:17:16 -0300

    Cleaner

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
-// BTN & Background
+// Get joke
 
 const button = document.querySelector(".get-joke-btn");
 const jokeP = document.querySelector(".joke");
 
-let url = "https://icanhazdadjoke.com"
-var options = { 
-    method: 'GET',
-    headers: {
-      "Accept" : "application/json" 
-    }
-  };
+let url = "https://icanhazdadjoke.com";
+var options = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
+};
 
-button.addEventListener("click", () => {
-    fetch(url, options)
-    .then(data => data.json())
-    .then(joke => jokeP.innerHTML = joke.joke)
-    .catch(err => alert("Something went wrong"))
-})
+const getJoke = () => {
+  fetch(url, options)
+    .then((data) => data.json())
+    .then((joke) => (jokeP.innerHTML = joke.joke))
+    .catch((err) => alert("Something went wrong"));
+};
 
-window.addEventListener("mousemove", e => {
-    let x = e.x / window.innerWidth * 255;
-    let y = e.y / window.innerHeight * 255;
+button.addEventListener("click", getJoke);
+getJoke();
 
-    // document.body.style.background = `rgba(${e.x / 4}, ${e.y / 4}, ${(e.x - e.y) / 4}, 0.8)`
+// Background color
 
-    document.body.style.background = `rgba(${x}, ${y}, ${e.x - e.y}, 0.8)`
-}
-)
-
-//init joke 
-
-fetch(url, options)
-.then(data => data.json())
-.then(joke => jokeP.innerHTML = joke.joke)
-.catch(err => alert("Something went wrong"))
+window.addEventListener("mousemove", (e) => {
+  let x = (e.x / window.innerWidth) * 255;
+  let y = (e.y / window.innerHeight) * 255;
+  console.log(window.innerWidth);
+  document.body.style.background = `rgba(${x}, ${y}, ${e.x - e.y}, 0.8)`;
+});
